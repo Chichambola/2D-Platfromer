7ae801d2a5aab69627d6e5e9934b0b0aafb14680
Chichambola <zhopacpu4koy@yandex.ru>
2025-10-14 19:05:31 +0700

    Adding requiered components to enemy

--- Assets/Enemy/Enemies/bear/BearScripts/Bear.cs
+++ Assets/Enemy/Enemies/bear/BearScripts/Bear.cs
@@ -1,8 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
+[RequireComponent(typeof(EnemyMover), typeof(EnemyRotator))]
 public class Bear : MonoBehaviour
 {
+    private EnemyMover _enemyMover;
+    private EnemyRotator _enemyRotator;
     
+    private void Awake()
+    {
+        _enemyMover = GetComponent<EnemyMover>();
+        _enemyRotator = GetComponent<EnemyRotator>();
+    }
 }
